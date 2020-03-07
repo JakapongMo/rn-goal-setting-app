71c47ce9f58e3ac7481a264933385d5ec14ab2c4
Jakapong <jakapong.klainongsuang@gmail.com>
2020-03-07 17:05:52 +0700

    Add modal overlay

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
-  TextInput,
-  Button,
-  ScrollView,
-  FlatList
+  FlatList,
+  Button
 } from 'react-native';
 
 import GoalItem from './components/GoalItem';
@@ -14,21 +11,30 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
+    console.log(goalTitle);
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
   };
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
       />
     </View>
   );
